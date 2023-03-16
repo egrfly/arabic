5a5280d606e163d8b427b09cdf46b9a4f1822efa
egrfly <egrfly@outlook.com>
2023-03-16 15:46:06 +0000

    fix: remove superfluous files

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <h1 className="text-primary">Learn Arabic.</h1>
-      <p>Use the links at the top of the page to choose a category to revise.</p>
+      <h1 className="mb-3">Learn Arabic.</h1>
+      <p className="text-primary">Use the links at the top of the page to choose a category to revise.</p>
     </>
   )
 }
